7e0467332d8e5cfd84096d9dcdc605817120f8fc
Zach Zura <zacharyzura@hotmail.com>
2018-12-20 18:43:13 -0600

    WORKING CODE!!!!!!!!!!!!!

---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CheeseMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel : CheeseCategory
+    {
+
+    }
+}
